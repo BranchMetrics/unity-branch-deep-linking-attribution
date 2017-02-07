96670906bbb12a388ac360c34c06c6378eec905c
Anton Arhunou <anton1980@tut.by>
2017-02-08 02:03:23 +0300

    versin 0.3.11

--- AndroidThirdParty/app/src/main/java/io/branch/unity/BranchCustomApplication.java
+++ AndroidThirdParty/app/src/main/java/io/branch/unity/BranchCustomApplication.java
@@ -2,12 +2,13 @@ package io.branch.unity;
 
 import android.app.Application;
 import io.branch.referral.Branch;
+import io.fabric.unity.android.FabricApplication;
 
 /**
  * Created by antonarhunou on 1/16/17.
  */
 
-public class BranchCustomApplication extends Application {
+public class BranchCustomApplication extends FabricApplication {
     @Override
     public void onCreate() {
         super.onCreate();
