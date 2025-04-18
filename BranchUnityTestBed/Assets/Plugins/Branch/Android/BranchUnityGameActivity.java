34abc697539fe3a38d9a18275f78d5083e279638
rob-gioia-branch <robert.gioia@branch.io>
2025-04-18 16:00:08 -0400

    Created new wrapper for custom UnityPlayerGameActivity class to fix Android build issue in newer versions of Unity
    Created new wrapper for custom UnityPlayerGameActivity class to fix Android build issue in newer versions of Unity

---
+++ BranchUnityTestBed/Assets/Plugins/Branch/Android/BranchUnityGameActivity.java
@@ -0,0 +1,12 @@
+package io.branch.unity;
+
+import com.unity3d.player.UnityPlayerGameActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class BranchUnityGameActivity extends UnityPlayerGameActivity {
+  protected void onCreate(Bundle savedInstanceState) {
+    // Calls UnityPlayerGameActivity.onCreate()
+    super.onCreate(savedInstanceState);
+  }
+}
